49bbf82d78739b8c30faa99bb6d491e68322ba71
root <root@fystorage1.(none)>
2012-04-16 17:27:01 +0800

    modify

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from urlgrabber import urlgrab
 url = 'http://i1.letvimg.com/vrs/201204/05/c3671b2ca6be47c6bcdb4d32e24f60ab.jpg'
 
 try:
-    urlgrab(url, '/tmp/' + 'image')
-    print 'download ok'
+    filename = urlgrab(url, '/tmp/' + 'image')
+    print('download %s ok' % filename)
 except Exception as (errno, strerr):
     print('download failed - ERRNO: %d ERR INFO: %s ' % (errno, strerr))
